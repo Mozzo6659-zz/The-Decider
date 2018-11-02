fa438b64808b6bc6f86c6722f145da28ec2de5e0
Mick Mossman <mossmanm@macbooks-macbook-pro.local>
2018-11-02 14:00:44 +1100

    Initial Commit

---
+++ The Decider/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  The Decider
+//
+//  Created by Mick Mossman on 2/11/18.
+//  Copyright © 2018 Mick Mossman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
