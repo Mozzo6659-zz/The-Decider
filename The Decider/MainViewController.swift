d2538679e15cf8b1736a942f7c2da39d145a5780
Mick Mossman <mossmanm@macbooks-macbook-pro.local>
2018-11-04 17:27:43 +1100

    Translating from Obj C

--- The Decider/MainViewController.swift
+++ The Decider/MainViewController.swift
@@ -7,14 +7,65 @@
 //
 
 import UIKit
+import AVFoundation
+import QuartzCore
 
 class MainViewController: UIViewController {
 
+    @IBOutlet weak var imgYouScore: UIImageView!
+    
+    @IBOutlet weak var imgMeScore: UIImageView!
+    
+    @IBOutlet weak var imgYouWinner: UIImageView!
+    
+    @IBOutlet weak var imgMeWinner: UIImageView!
+    
+    @IBOutlet weak var imgArrow: UIImageView!
+    
+    var winImages : [String] = []
+    var audioPlayer : AVAudioPlayer!
+    
+    var winnerisMe = false
+    var winnerisYou = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadImages()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    //MARK:- Build
+    func initPlayer() {
+        
+    }
+    
+    func playSoundWithFile(filename:String) {
+        if let sound = NSDataAsset(name: filename) {
+            do {
+                audioPlayer = try AVAudioPlayer(data: sound.data)
+                audioPlayer.play()
+            }catch{
+                print("Error playng sound")
+            }
+//            do {
+//                try! AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
+//                try! AVAudioSession.sharedInstance().setActive(true)
+//                try alarmAudioPlayer = AVAudioPlayer(data: sound.data, fileTypeHint: AVFileTypeWAVE)
+//                alarmAudioPlayer!.play()
+//            } catch {
+//                print("error initializing AVAudioPlayer")
+//            }
+        }
+    }
+    
+    func loadImages() {
+        for i in 1 ... 12 {
+            winImages.append(String(format: "winner%d", i))
+        }
+    }
+    //MARK:- IBActions
+    
+    @IBAction func spinArrow(_ sender: Any) {
+    }
 }
 
